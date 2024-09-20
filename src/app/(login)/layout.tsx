1c9142dd8fcaa6e50e355c8cabf18c31e3a645b8
maiconlara <maiconluan.lara@yahoo.com.br>
2024-09-20 16:14:50 -0300

    refactor: adding local font in login layout

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -1,12 +1,42 @@
 import {Header} from "@/components/ui";
 import type {Metadata} from "next";
-
+import localFont from "next/font/local";
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "Blue Empreendimentos",
   };
 }
 
+const Sora = localFont({
+  src: [
+    {
+      path: "../../assets/fonts/Sora-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/Sora-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/Sora-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/Sora-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/Sora-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-sora",
+});
 export default async function RootLayout({
   children,
 }: {
@@ -14,9 +44,8 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      
       <body
-        className={`flex min-h-screen w-screen flex-col items-start justify-start overflow-x-hidden font-sora bg-primary`}
+        className={` ${Sora.variable} flex min-h-screen w-screen flex-col items-start justify-start overflow-x-hidden font-sora bg-primary`}
       >
         <Header />
 
