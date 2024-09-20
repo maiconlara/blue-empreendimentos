41a0e5c432366d4d70dc245dfab54d28101d38ce
maiconlara <maiconluan.lara@yahoo.com.br>
2024-09-20 16:15:01 -0300

    refactor: changing loading icon in submit button

--- src/components/ui/submit-button.tsx
+++ src/components/ui/submit-button.tsx
@@ -1,4 +1,7 @@
-import { IconLoader2 } from "@tabler/icons-react";
+import {
+  RiLoader4Fill,
+ 
+} from "@remixicon/react";
 
 import {Button} from "./button";
 
@@ -17,11 +20,11 @@ export const SubmitButton = ({
     <Button
       type="submit"
       form={form}
-      className={`${isLoading ? "cursor-wait" : "cursor-pointer"} flex bg-[#0B76BB] rounded-full hover:bg-[#0B76BB]/80 transition-all w-full px-4 h-12 items-center justify-center  text-white-off font-regular text-[14px] leading-4`}
+      className={`${isLoading ? "cursor-default" : "cursor-pointer"} flex bg-[#0B76BB] rounded-full hover:bg-[#0B76BB]/80 transition-all w-full px-4 h-12 items-center justify-center  text-white-off font-regular text-[14px] leading-4`}
       disabled={isLoading}
     >
       {isLoading ? (
-          <IconLoader2 className="h-5 w-5 text-white animate-spin" />
+          <RiLoader4Fill className="h-5 w-5 text-white animate-spin" />
       ) : (
         children
       )}
